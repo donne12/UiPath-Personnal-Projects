228b91da54a1da173520350fda6fbc92acc8bb1d
Dieudonné Koffi ESIAKU <70475139+donne12@users.noreply.github.com>
2023-07-05 16:30:48 +0200

    first commit

---
+++ UIAutomation/.local/install/CodedWorkflow.cs
@@ -0,0 +1,18 @@
+using System;
+using UiPath.CodedWorkflows;
+using UiPath.UIAutomationNext.API.Contracts;
+using UiPath.UIAutomationNext.API.Models;
+using UiPath.UIAutomationNext.Enums;
+
+namespace UIAutomation
+{
+    public class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService)};
+        }
+
+        protected UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService uiAutomation { get => serviceContainer.Resolve<UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService>() ; }
+    }
+}
